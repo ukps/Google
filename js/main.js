1fd7983b15df2ca11f377afbff9ebc32fa972f9b
Heithem Kacem <58637738+ukps@users.noreply.github.com>
2020-03-12 23:33:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,15 +52,7 @@ function myFunction() {
 	};
 	fullHeight();
 
-	// loader
-	var loader = function () {
-		setTimeout(function () {
-			if ($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
+
 
 	// Scrollax
 	$.Scrollax();
